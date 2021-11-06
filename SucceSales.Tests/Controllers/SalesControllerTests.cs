c7b6fea8aed8f22677bc271b1dced78da6fcd88b
Samuel Peralta Antequera <samuel11.1.93@gmail.com>
2021-11-06 16:21:05 +0100

    Added tests to get method and updated some tests names. closes #22

--- SucceSales.Tests/Controllers/SalesControllerTests.cs
+++ SucceSales.Tests/Controllers/SalesControllerTests.cs
@@ -8,12 +8,13 @@ namespace SucceSales.Tests.Controllers
     using SucceSales.Controllers;
     using SucceSales.Domain.DomainModel;
     using System;
+    using System.Threading.Tasks;
     using Xunit;
-
 
     public class SalesControllerTests
     {
         private readonly SalesController _salesController;
+        private readonly Sales _firstSale = new Sales{Id = 1, ProductId = 1, ProductName = "Banana", Quantity = 3, Date = DateTime.Now};
 
         public SalesControllerTests()
         {
@@ -22,13 +23,13 @@ namespace SucceSales.Tests.Controllers
                 .UseInMemoryDatabase("AllSales").Options;
 
             var salesContext = new SalesContext(options);
-            salesContext.AllSales.Add(new Sales{Id = 1, ProductId = 1, ProductName = "Banana", Quantity = 3, Date = DateTime.Now});
+            salesContext.AllSales.Add(_firstSale);
 
             _salesController = new SalesController(salesLogger.Object, salesContext);
         }
 
         [Fact]
-        public void TestPostSale_Returns201()
+        public void TestPostSale_PostNewSale_ReturnsCreated201()
         {
             var expectedSale = new Sales{Id = 2, ProductId = 1, ProductName = "Banana", Quantity = 4, Date = DateTime.Now};
             
@@ -38,6 +39,41 @@ namespace SucceSales.Tests.Controllers
             var actionResult = (CreatedAtActionResult)result.Result.Result;
             Check.That(actionResult.StatusCode).IsEqualTo(201);
             Check.That(actionResult.Value).IsEqualTo(expectedSale).And.IsInstanceOf<Sales>();
-        }        
+        }
+
+        [Fact]
+        public void TestPostSale_PostExistingSale_ReturnsException()
+        {
+            var expectedSale = new Sales{Id = 1, ProductId = 1, ProductName = "Banana", Quantity = 4, Date = DateTime.Now};
+            
+            var result = _salesController.PostSale(expectedSale);
+
+            Check.That(result.Status).IsEqualTo(TaskStatus.Faulted);
+            Check.That(result.Exception.HResult).IsEqualTo(-2146233088);
+        }
+
+        [Fact]
+        public void TestGetSale_SearchForFirstSale_ReturnsFirstSale()
+        {
+            var result = _salesController.GetSale(1);
+
+            Check.That(result.Result).IsInstanceOf<ActionResult<Sales>>();
+            var actionResult = (ActionResult<Sales>)result.Result;
+            Check.That(actionResult.Value).IsEqualTo(_firstSale).And.IsInstanceOf<Sales>();
+        }
+
+        [Fact]
+        public void TestGetSale_SearchForNonExistingSale_ReturnsNotFound404()
+        {
+            var result = _salesController.GetSale(2);
+
+            Check.That(result.Result).IsInstanceOf<ActionResult<Sales>>();
+            var actionResult = (ActionResult<Sales>)result.Result;
+            Check.That(actionResult.Value).IsEqualTo(null);
+
+            Check.That(actionResult.Result).IsInstanceOf<NotFoundResult>();
+            var notFoundResult = (NotFoundResult)actionResult.Result;
+            Check.That(notFoundResult.StatusCode).IsEqualTo(404);
+        }
     }
 }
